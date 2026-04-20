61013411e6529df4a3b3e51bf2adf39b4708cd8c
Kha Pham <khapham0027@gmail.com>
2026-04-20 15:41:50 +0800

    Started creating the 2 functions needed for jumble, one to choose the word and one to actually interact with the user.

---
+++ Jumble.py
@@ -0,0 +1,31 @@
+import random
+
+
+def choose():
+    words = ['cat', 'dog']
+    pick = random.choice(words)
+    return pick
+
+def jumble(words):
+    random_word = random.sample(words, len(words))
+    jumbled = ''.join(random_word)
+    print(jumbled)
+
+def play_jumble():
+    while True:
+        while True:
+            try:
+                rounds_jumble = int(input("How many rounds of Jumble would you like to play? "))
+                print("You have chosen to play {rounds_jumble} rounds")
+                break
+            except ValueError:
+                rounds_jumble = int(input("How many rounds of Jumble would you like to play? "))
+        for i in rounds_jumble:
+            score_jumble = 0
+            picked_word = choose()
+            jumbled_word = jumble(picked_word)
+            answer = input("What is the word")
+            if answer == jumbled_word:
+                score_jumble += 1
+            else:
+                print("incorrect")
